392b35c334c9a78e809ace7c844950da19b8f775
mgamboa <gamboa.martin@gmail.com>
2022-04-21 11:18:32 -0500

    v1

--- base/conexion.php
+++ base/conexion.php
@@ -13,6 +13,14 @@ class conexion{
      */
     public function __construct(){
         $error = new errores();
+        if(!class_exists(generales::class)){
+            $data = file_get_contents('././config/generales.php.example');
+            $error = $error->error(mensaje: 'Error no existe clase config\\generales',data: $data,
+                params: get_defined_vars());
+            print_r($error);
+            exit;
+        }
+
         if(!class_exists(database::class)){
             $data = file_get_contents((new generales())->path_base.'config/database.php.example');
             $error = $error->error(mensaje: 'Error no existe clase config\\database',data: $data,
