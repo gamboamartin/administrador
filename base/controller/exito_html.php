a28536b2f94aab6f55daf341c3492fb9da4b1904
mgamboa <gamboa.martin@gmail.com>
2022-06-17 13:12:57 -0500

    v1.67.17 Se integran ajusten en mensajeria de exito

--- base/controller/exito_html.php
+++ base/controller/exito_html.php
@@ -1,5 +1,6 @@
 <?php
 namespace base\controller;
+use base\orm\validaciones;
 use gamboamartin\errores\errores;
 
 class exito_html extends base_html {
@@ -13,18 +14,49 @@ class exito_html extends base_html {
         return '<button type="button" class="btn btn-success" data-toggle="collapse" data-target="#msj_exito">Detalle</button>';
     }
 
-    public function mensaje(array $mensaje_exito): string
+    /**
+     * Asigna el mensaje de exito a un p
+     * @version 1.67.17
+     * @param array $mensaje_exito Datos con mensaje
+     * @return string|array
+     */
+    private function mensaje(array $mensaje_exito): string|array
     {
+        $keys = array('mensaje');
+        $valida = (new validaciones())->valida_existencia_keys(keys: $keys, registro: $mensaje_exito);
+        if(errores::$error){
+            $fix = 'Debe existir mensaje_exito[mensaje]';
+            return $this->error->error(mensaje: 'Error al integrar mensaje', data: $valida, fix: $fix);
+        }
+
         return '<p class="mb-0">'.$mensaje_exito['mensaje'] . '</p>';
     }
 
+    /**
+     * Integra el trazado de todos los mensajes de exito
+     * @version 1.67.17
+     * @param array $mensajes_exito Conjunto de mensajes cargados en un SESSION
+     * @return array|string
+     */
     private function mensajes(array $mensajes_exito): array|string
     {
         $html = '';
         foreach ($mensajes_exito as $mensaje_exito) {
+            if(!is_array($mensaje_exito)){
+                $fix = 'mensajes_exito debe tener la siguiente forma $mensaje_exito[][mensaje] = mensaje';
+                return $this->error->error(mensaje: 'Error $mensaje_exito debe ser un array', data: $mensaje_exito,
+                    fix: $fix);
+            }
+            $keys = array('mensaje');
+            $valida = (new validaciones())->valida_existencia_keys(keys: $keys, registro: $mensaje_exito);
+            if(errores::$error){
+                $fix = 'mensajes_exito debe tener la siguiente forma $mensaje_exito[][mensaje] = mensaje ';
+                $fix .= 'Debe existir mensaje_exito[mensaje]';
+                return $this->error->error(mensaje: 'Error al integrar mensaje', data: $valida, fix: $fix);
+            }
             $mensaje_html = $this->mensaje(mensaje_exito: $mensaje_exito);
             if(errores::$error){
-                return $this->error->error('Error al generar mensaje', $mensaje_html);
+                return $this->error->error(mensaje: 'Error al generar mensaje', data: $mensaje_html);
             }
             $html .=      $mensaje_html;
         }
@@ -77,12 +109,18 @@ class exito_html extends base_html {
         return $exito_transaccion;
     }
 
-    public function mensajes_collapse(array $mensajes_exito): array|string
+    /**
+     * Genera los mensajes para se mostrados en html
+     * @version 1.67.17
+     * @param array $mensajes_exito Conjunto de mensajes obtenidos se SESSION
+     * @return array|string Salida html de mensajes en success
+     */
+    private function mensajes_collapse(array $mensajes_exito): array|string
     {
 
         $mensajes = $this->mensajes(mensajes_exito: $mensajes_exito);
         if(errores::$error){
-            return $this->error->error('Error al generar mensajes', $mensajes);
+            return $this->error->error(mensaje: 'Error al generar mensajes', data: $mensajes);
         }
         return  '<div class="collapse" id="msj_exito">'.$mensajes.'</div>';
 
