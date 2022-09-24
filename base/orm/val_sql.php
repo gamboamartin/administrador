3810b29eb8797de876536e7e5889b5b65e419055
mgamboa <gamboa.martin@gmail.com>
2022-09-24 11:19:29 -0500

    v1.490.49 Se integra valida cod 3

--- base/orm/val_sql.php
+++ base/orm/val_sql.php
@@ -46,6 +46,25 @@ class val_sql extends validaciones {
     }
 
     /**
+     * Valida que un conjunto de ids existan y sean validos para una transaccion de un modelo
+     * @param array $keys_ids Campos de tipo ids a validar
+     * @param array $registro Registro a verificar en conjunto de los keys id definidos
+     * @return bool|array
+     * @version 1.443.48
+     */
+    private function ids(array $keys_ids, array $registro ): bool|array
+    {
+        foreach($keys_ids as $campo){
+            $verifica = $this->verifica_id(campo: $campo,keys_ids: $keys_ids,registro: $registro);
+            if(errores::$error){
+                return $this->error->error(mensaje: 'Error al verificar campo ids',data:  $verifica);
+            }
+
+        }
+        return true;
+    }
+
+    /**
      * Limpia y valida unj elemento vacio
      * @param string $campo campo a verificar
      * @param array $keys_obligatorios Conjunto de campos obligatorios definidos en el modelo
@@ -89,39 +108,11 @@ class val_sql extends validaciones {
         return true;
     }
 
-    /**
-     * Verifica los campos obligatorios esten bien ajustados
-     * @param array $keys_obligatorios Conjunto de keys a verificar nativos de modelo
-     * @param array $registro Registro a validar
-     * @return bool|array
-     * @version 1.439.48
-     */
-    private function obligatorios(array $keys_obligatorios, array $registro): bool|array
+
+    private function cod_3_mayusc(array $keys_cod_3_mayus, array $registro ): bool|array
     {
-        $existe = $this->existe(keys_obligatorios: $keys_obligatorios, registro: $registro);
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error al validar campos no existe', data: $existe);
-        }
-        $vacio = $this->vacio(keys_obligatorios: $keys_obligatorios, registro: $registro);
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error al validar campo vacio', data: $vacio);
-        }
-
-
-        return true;
-    }
-
-    /**
-     * Valida que un conjunto de ids existan y sean validos para una transaccion de un modelo
-     * @param array $keys_ids Campos de tipo ids a validar
-     * @param array $registro Registro a verificar en conjunto de los keys id definidos
-     * @return bool|array
-     * @version 1.443.48
-     */
-    private function ids(array $keys_ids, array $registro ): bool|array
-    {
-        foreach($keys_ids as $campo){
-            $verifica = $this->verifica_id(campo: $campo,keys_ids: $keys_ids,registro: $registro);
+        foreach($keys_cod_3_mayus as $campo){
+            $verifica = $this->verifica_cod_3_mayusc(campo: $campo,keys_cod_3_mayus: $keys_cod_3_mayus,registro: $registro);
             if(errores::$error){
                 return $this->error->error(mensaje: 'Error al verificar campo ids',data:  $verifica);
             }
@@ -152,6 +143,28 @@ class val_sql extends validaciones {
         $data->key = $key_r;
         $data->tipo_campo = $tipo_campo_r;
         return $data;
+    }
+
+    /**
+     * Verifica los campos obligatorios esten bien ajustados
+     * @param array $keys_obligatorios Conjunto de keys a verificar nativos de modelo
+     * @param array $registro Registro a validar
+     * @return bool|array
+     * @version 1.439.48
+     */
+    private function obligatorios(array $keys_obligatorios, array $registro): bool|array
+    {
+        $existe = $this->existe(keys_obligatorios: $keys_obligatorios, registro: $registro);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al validar campos no existe', data: $existe);
+        }
+        $vacio = $this->vacio(keys_obligatorios: $keys_obligatorios, registro: $registro);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al validar campo vacio', data: $vacio);
+        }
+
+
+        return true;
     }
 
     public function tabla(string $tabla): bool|array
@@ -226,87 +239,6 @@ class val_sql extends validaciones {
     }
 
     /**
-     * Valida los elementos de un checked
-     * @param string $campo Nombre del campo
-     * @param array $keys_checked Key para validar checked
-     * @param array $registro Registro a validar
-     * @return bool|array
-     * @version 1.443
-     */
-    private function verifica_chk(string $campo, array $keys_checked, array $registro): bool|array
-    {
-        $campo_r = $this->txt_valido($campo);
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error al validar campo valor',data: $campo_r);
-        }
-        $existe = $this->existe(keys_obligatorios: $keys_checked,registro:  $registro);
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error al validar si existe',data: $existe);
-        }
-
-        if(!isset($registro[$campo_r])){
-            return $this->error->error(mensaje: 'Error debe existir en registro '.$campo_r,data: $registro);
-        }
-
-        if((string)$registro[$campo_r] !== 'activo' && (string)$registro[$campo_r]!=='inactivo' ){
-            return $this->error->error(mensaje: 'Error $registro['.$campo_r.'] debe ser activo o inactivo',
-                data: $registro);
-        }
-        return true;
-    }
-
-    /**
-     * Verifica la estructura de un registro conforme a la configuracion del modelo
-     * @param array $campos_obligatorios Conjunto de campos obligatorios de un modelo
-     * @param array $registro Registro a verificar en conjunto de los keys id definidos
-     * @param string $tabla Tabla a verificar
-     * @param array $tipo_campos Campos con tipificacion desde modelo
-     * @return bool|array
-     * @version 1.444.48
-     */
-    private function verifica_estructura(array $campos_obligatorios, array $registro, string $tabla,
-                                         array $tipo_campos): bool|array
-    {
-        $valida_campo_obligatorio = $this->valida_campo_obligatorio(campos_obligatorios: $campos_obligatorios,
-            registro: $registro,tabla:  $tabla);
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error el campo al validar campos obligatorios de registro '.$tabla,
-                data: $valida_campo_obligatorio);
-        }
-
-        $valida_estructura = (new val_sql())->valida_estructura_campos(registro: $registro, tipo_campos: $tipo_campos);
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error el campo al validar estructura ', data: $valida_estructura);
-        }
-        return true;
-    }
-
-    /**
-     * Verifica que existe un campo de tipo id en una transaccion
-     * @param string $campo Campo a validar
-     * @param array $keys_ids Keys a validar
-     * @param array $registro Registro a verificar
-     * @return bool|array
-     * @version 1.439.49
-     */
-    private function verifica_id(string $campo, array $keys_ids, array $registro): bool|array
-    {
-        $campo_r = $this->campo_existe(campo: $campo,keys_ids: $keys_ids,registro: $registro);
-        if(errores::$error){
-            return $this->error->error(mensaje: 'Error al verificar campo ids', data: $campo_r);
-        }
-        if(!isset($registro[$campo_r])){
-            return $this->error->error(mensaje: 'Error no existe '.$campo_r.' en registro', data: $registro);
-        }
-
-        if(!preg_match($this->patterns['id'], $registro[$campo_r])){
-            return $this->error->error(mensaje: 'Error $registro['.$campo_r.'] es invalido',
-                data: array($registro[$campo_r],$this->patterns['id']));
-        }
-        return true;
-    }
-
-    /**
      * Valida un registro previo a la alta en bd
      * @param array $campos_obligatorios Campos definidos en el modelo como obligatorios
      * @param modelo $modelo Modelo a validar el alta
@@ -367,7 +299,7 @@ class val_sql extends validaciones {
      * @version 1.444.48
      */
     private function valida_estructura_campos(array $registro, array $tipo_campos, array $keys_checked = array(),
-                                             array $keys_ids = array(), array $keys_obligatorios = array()): array|bool
+                                              array $keys_ids = array(), array $keys_obligatorios = array()): array|bool
     {
 
 
@@ -389,6 +321,104 @@ class val_sql extends validaciones {
         }
 
 
+        return true;
+    }
+
+    /**
+     * Valida los elementos de un checked
+     * @param string $campo Nombre del campo
+     * @param array $keys_checked Key para validar checked
+     * @param array $registro Registro a validar
+     * @return bool|array
+     * @version 1.443
+     */
+    private function verifica_chk(string $campo, array $keys_checked, array $registro): bool|array
+    {
+        $campo_r = $this->txt_valido($campo);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al validar campo valor',data: $campo_r);
+        }
+        $existe = $this->existe(keys_obligatorios: $keys_checked,registro:  $registro);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al validar si existe',data: $existe);
+        }
+
+        if(!isset($registro[$campo_r])){
+            return $this->error->error(mensaje: 'Error debe existir en registro '.$campo_r,data: $registro);
+        }
+
+        if((string)$registro[$campo_r] !== 'activo' && (string)$registro[$campo_r]!=='inactivo' ){
+            return $this->error->error(mensaje: 'Error $registro['.$campo_r.'] debe ser activo o inactivo',
+                data: $registro);
+        }
+        return true;
+    }
+
+    private function verifica_cod_3_mayusc(string $campo, array $keys_cod_3_mayus, array $registro): bool|array
+    {
+        $campo_r = $this->campo_existe(campo: $campo,keys_ids: $keys_cod_3_mayus,registro: $registro);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al verificar campo ids', data: $campo_r);
+        }
+        if(!isset($registro[$campo_r])){
+            return $this->error->error(mensaje: 'Error no existe '.$campo_r.' en registro', data: $registro);
+        }
+
+        if(!preg_match($this->patterns['id'], $registro[$campo_r])){
+            return $this->error->error(mensaje: 'Error $registro['.$campo_r.'] es invalido',
+                data: array($registro[$campo_r],$this->patterns['id']));
+        }
+        return true;
+    }
+
+    /**
+     * Verifica la estructura de un registro conforme a la configuracion del modelo
+     * @param array $campos_obligatorios Conjunto de campos obligatorios de un modelo
+     * @param array $registro Registro a verificar en conjunto de los keys id definidos
+     * @param string $tabla Tabla a verificar
+     * @param array $tipo_campos Campos con tipificacion desde modelo
+     * @return bool|array
+     * @version 1.444.48
+     */
+    private function verifica_estructura(array $campos_obligatorios, array $registro, string $tabla,
+                                         array $tipo_campos): bool|array
+    {
+        $valida_campo_obligatorio = $this->valida_campo_obligatorio(campos_obligatorios: $campos_obligatorios,
+            registro: $registro,tabla:  $tabla);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error el campo al validar campos obligatorios de registro '.$tabla,
+                data: $valida_campo_obligatorio);
+        }
+
+        $valida_estructura = (new val_sql())->valida_estructura_campos(registro: $registro, tipo_campos: $tipo_campos);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error el campo al validar estructura ', data: $valida_estructura);
+        }
+        return true;
+    }
+
+    /**
+     * Verifica que existe un campo de tipo id en una transaccion
+     * @param string $campo Campo a validar
+     * @param array $keys_ids Keys a validar
+     * @param array $registro Registro a verificar
+     * @return bool|array
+     * @version 1.439.49
+     */
+    private function verifica_id(string $campo, array $keys_ids, array $registro): bool|array
+    {
+        $campo_r = $this->campo_existe(campo: $campo,keys_ids: $keys_ids,registro: $registro);
+        if(errores::$error){
+            return $this->error->error(mensaje: 'Error al verificar campo ids', data: $campo_r);
+        }
+        if(!isset($registro[$campo_r])){
+            return $this->error->error(mensaje: 'Error no existe '.$campo_r.' en registro', data: $registro);
+        }
+
+        if(!preg_match($this->patterns['id'], $registro[$campo_r])){
+            return $this->error->error(mensaje: 'Error $registro['.$campo_r.'] es invalido',
+                data: array($registro[$campo_r],$this->patterns['id']));
+        }
         return true;
     }
 
