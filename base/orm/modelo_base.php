d4c4f525d6f7f64341a629245ff8c163ffaf1a83
mgamboa <gamboa.martin@gmail.com>
2022-10-04 09:53:30 -0500

    v1.514.51 Se integra com models

--- base/orm/modelo_base.php
+++ base/orm/modelo_base.php
@@ -626,6 +626,7 @@ class modelo_base{ //PRUEBAS EN PROCESO //DOCUMENTACION EN PROCESO
         $namespaces[]  = 'gamboamartin\\organigrama\\models\\';
         $namespaces[]  = 'gamboamartin\\direccion_postal\\models\\';
         $namespaces[]  = 'gamboamartin\\cat_sat\\models\\';
+        $namespaces[]  = 'gamboamartin\\comercial\\models\\';
 
         $es_namespace_especial_como_mis_inges = false;
         foreach ($namespaces as $namespace) {
