206412f82a9fa53eb7c8cb34a7e1b44e5024fdb0
mgamboa <gamboa.martin@gmail.com>
2023-05-22 17:17:28 -0600

    v10.57.3 Se integra notificacon in base init

--- base/orm/modelo_base.php
+++ base/orm/modelo_base.php
@@ -855,6 +855,7 @@ class modelo_base{ //PRUEBAS EN PROCESO //DOCUMENTACION EN PROCESO
         $namespaces[]  = 'gamboamartin\\im_registro_patronal\\models\\';
         $namespaces[]  = 'gamboamartin\\importador\\models\\';
         $namespaces[]  = 'gamboamartin\\proceso\\models\\';
+        $namespaces[]  = 'gamboamartin\\notificaciones\\models\\';
         $namespaces[]  = 'tglobally\\tg_nomina\\models\\';
         $namespaces[]  = 'tglobally\\tg_empleado\\models\\';
         $namespaces[]  = 'tglobally\\tg_notificacion\\models\\';
