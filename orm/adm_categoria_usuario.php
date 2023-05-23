2fb2db0cef3c8c33c327c68c2ec87c4f27c9ead9
Kevin <kevin.acuna@ivitec.com.mx>
2023-05-23 16:52:19 -0500

    v10.62.3 Se integra relacion para columnas en categoria usuario

--- orm/adm_categoria_usuario.php
+++ orm/adm_categoria_usuario.php
@@ -6,7 +6,8 @@ use PDO;
 class adm_categoria_usuario extends _modelo_parent {
     public function __construct(PDO $link){
         $tabla = 'adm_categoria_usuario';
-        $columnas = array($tabla=>false);
+        $columnas = array($tabla=>false, 'adm_usuario' => $tabla, 'adm_categoria_sistema' => $tabla,
+            'adm_categoria' => 'adm_categoria_sistema', 'adm_sistema' => 'adm_categoria_sistema');
         parent::__construct(link: $link, tabla: $tabla, columnas: $columnas);
         $this->NAMESPACE = __NAMESPACE__;
     }
